b4a531996b6465b2f5e908d8fbda96ab9dccb2ba
Vamsi Balakiran Polisetty <vamsibalakiranpolisetty@gmail.com>
2025-08-06 15:25:47 +0530

    🚀 Auto-deploy update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Vamsi-Portfolio/',
+  base: '/V amsi-Portfolio/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
